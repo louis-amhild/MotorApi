3acb6dd6796bcd68884afa25cabf5efa15f33991
louis <la@motes.dk>
2018-01-20 00:52:02 +0100

    Out comment RFID dev

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #/usr/bin/bash
 
-sudo rm /dev/spidev0.0 && sudo ln /dev/spidev1.0 /dev/spidev0.0
+#sudo rm /dev/spidev0.0 && sudo ln /dev/spidev1.0 /dev/spidev0.0
 
 sudo npm --prefix /var/www/html/MotorApi run start
 
